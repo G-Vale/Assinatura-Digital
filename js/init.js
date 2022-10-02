f055b67389ac17772f118203319750c7a7f5c5e2
G-Vale <gabrielcrc.pj@gmail.com>
2022-10-01 21:06:00 -0300

    ate q fim

---
+++ js/init.js
@@ -0,0 +1,13 @@
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkvIQSpyrRf3aFm2VxkQf2slvWg6ZP_vc",
+    authDomain: "assinatura-digital-g-vale.firebaseapp.com",
+    projectId: "assinatura-digital-g-vale",
+    storageBucket: "assinatura-digital-g-vale.appspot.com",
+    messagingSenderId: "926707083676",
+    appId: "1:926707083676:web:d26bd2e0c9e8d60059c059"
+  };
+
+firebase.initializeApp(firebaseConfig)
